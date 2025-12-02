19014a04d0a39aad9c043277392ca18f7556996b
cryptokapa44 <cryptokapa44@gmail.com>
2025-12-02 11:06:15 +0200

    Update site.js

--- docs/scripts/site.js
+++ docs/scripts/site.js
@@ -7,7 +7,7 @@ function formatNumber(num) {
   if (num >= 1_000_000_000) return (num / 1_000_000_000).toFixed(1) + "B";
   if (num >= 1_000_000) return (num / 1_000_000).toFixed(1) + "M";
   if (num >= 1_000) return (num / 1_000).toFixed(1) + "K";
-  return num.toString();
+  return Math.round(num).toString(); // This line is updated
 }
 
 // Format and inject values without animation
